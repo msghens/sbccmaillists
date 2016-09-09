e10aeec8a5c664a404b0bd0454c9aa7c808318d9
Michael Ghens <mghens@sbcc.edu>
2016-09-09 16:36:33 -0700

    End of week commit

---
+++ list-shell.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+from google_group_lib import googLib
+
+#
+#  Mail List Shell. Use this file to create new lists.
+#
+
+# Name of mail list. This should  mirror Group email address in settings
+
+listname = 'list@sbcc.edu or list-group@pipeline.sbcc.edu'
+
+
+# next is the sql. This should be a query that returns email address
+# This should call a view in schema sbcc_listsrv
+sql = "select 1 from dual"
+
+
+
+
+
--- ora_list.py
+++ ora_list.py
@@ -1,3 +1,32 @@
 """
-Oracle access for lists.
+Oracle access for  mail listlists.
+http://www.oracle.com/technetwork/articles/dsl/prez-transactions-lobs-089563.html
 """
+
+import cx_Oracle
+from secrets import banHOST,banUSER,banPASS,banPORT,banSID
+
+
+class ora_sbcc_lists(object):
+	
+	
+	def __enter__(self):
+		dsn_tns = cx_Oracle.makedsn(banHOST, banPORT, banSID)
+		self.__db = cx_Oracle.connect(banUSER, banPASS, dsn_tns)
+		self.__cursor = self.__db.cursor()
+		return self
+		
+	def __exit__(self,type,value,traceback):
+		self.__db.close()
+		
+	def get_ban_list(self,sql):
+		
+		select_sql = sql
+		
+		#~ self.__db.begin()
+		self.__cursor.execute(sql)
+		#~ D = dict(self.__cursor.fetchall())
+		#~ return D
+		return self.__cursor.fetchall()
+		
+		
